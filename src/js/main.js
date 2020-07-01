abc98dfa4741e7eed8e075a82b11c6b4f29f5a7e
Vladimir <avevanguard@gmail.com>
2020-07-01 18:05:12 +0300

    + burger menu for tablets

--- src/js/main.js
+++ src/js/main.js
@@ -8,6 +8,7 @@ import calc from './modules/calc';
 import filter from './modules/filter';
 import pictureSize from './modules/pictureSize';
 import accordion from './modules/accordion';
+import burger from './modules/burger';
 
 document.addEventListener('DOMContentLoaded', () => { 
     "use strict";
@@ -30,6 +31,8 @@ document.addEventListener('DOMContentLoaded', () => {
     pictureSize('.sizes-block');
 
     accordion('.accordion-heading');
+
+    burger('.burger-menu', '.burger');
 
 
     
